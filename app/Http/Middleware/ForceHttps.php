31dfb6298a9ef4170d7e84b2bce60d4e734d0f85
Samit Koyom <samitkoyom@gmail.com>
2025-05-17 16:03:05 +0700

    update for https #2

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -4,13 +4,17 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 
 class ForceHttps
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->secure() && app()->environment('production')) {
-            return redirect()->secure($request->getRequestUri());
+        if (App::environment('production')) {
+            // ตรวจสอบ X-Forwarded-Proto หรือ secure connection
+            if (!$request->isSecure() && !$request->header('X-Forwarded-Proto') === 'https') {
+                return redirect()->secure($request->getRequestUri());
+            }
         }
 
         return $next($request);
